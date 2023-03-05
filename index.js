915c535e8cd034ff823d245b2881cff3ae31dcd6
SaBah2790 <114356296+SaBah2790@users.noreply.github.com>
2023-03-05 18:27:59 +0100

    updated images, added the conversion to and from celsius-fahrenheit, chanced link colour and modified icon size

--- index.js
+++ index.js
@@ -20,18 +20,16 @@ function someWeather(response) {
 	let wind = document.querySelector("#wind");
 	let icon = document.querySelector("#weather-icon");
 	newCity.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
-	degrees.innerHTML = `${degreesNow} &deg;`;
+	degrees.innerHTML = degreesNow;
 	description.innerHTML = response.data.weather[0].description;
 	humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
 	wind.innerHTML = `Windspeed: ${Math.round(response.data.wind.speed)} km/h`;
 	icon.setAttribute("src", `images/${response.data.weather[0].icon}.png`);
-	console.log(response.data.weather[0].icon);
 }
 //city change//
 function defaultCity(city) {
 	let apiKey = "0ebc654fccbc00189d5408f3d6f15b08";
-	let unit = ["metric", "imperial"];
-	let apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit[0]}&appid=${apiKey}`;
+	let apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 	axios.get(apiCall).then(someWeather);
 }
 function cityLookUp(event) {
@@ -46,9 +44,8 @@ searchForm.addEventListener("submit", cityLookUp);
 function weatherMyLocation(position) {
 	let latitude = position.coords.latitude;
 	let longitude = position.coords.longitude;
-	let unit = ["metric", "imperial"];
 	let apiKey = "0ebc654fccbc00189d5408f3d6f15b08";
-	let apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit[0]}`;
+	let apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 	axios.get(apiCall).then(someWeather);
 }
 function myLocation(event) {
@@ -56,4 +53,37 @@ function myLocation(event) {
 }
 let myCurrentPosition = document.querySelector("#current-location");
 myCurrentPosition.addEventListener("click", myLocation);
+
+//weather degrees conversion
+let currentUnit = "celsius"; // initialize current unit to Celsius
+
+function convertTemperature(event) {
+	event.preventDefault();
+	let degreesCurrent = document.querySelector("#degrees");
+
+	if (currentUnit === "celsius") {
+		// convert Celsius to Fahrenheit
+		celsiusDeg.classList.remove("active");
+		fahrenheitDeg.classList.add("active");
+		let celsiusValue = parseFloat(degreesCurrent.innerHTML);
+		let fahrenheitValue = (celsiusValue * 9) / 5 + 32;
+		degreesCurrent.innerHTML = Math.round(fahrenheitValue);
+		currentUnit = "fahrenheit";
+	} else {
+		// convert Fahrenheit to Celsius
+		fahrenheitDeg.classList.remove("active");
+		celsiusDeg.classList.add("active");
+		let fahrenheitValue = parseFloat(degreesCurrent.innerHTML);
+		let celsiusValue = ((fahrenheitValue - 32) * 5) / 9;
+		degreesCurrent.innerHTML = Math.round(celsiusValue);
+		currentUnit = "celsius";
+	}
+}
+
+let celsiusDeg = document.querySelector("#celsius");
+celsiusDeg.addEventListener("click", convertTemperature);
+
+let fahrenheitDeg = document.querySelector("#fahrenheit");
+fahrenheitDeg.addEventListener("click", convertTemperature);
+
 defaultCity("Stockholm");
